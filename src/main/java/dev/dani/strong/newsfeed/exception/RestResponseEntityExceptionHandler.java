baaf682e113c0ff2044ced71ccdd9f6a187aae09
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2023-05-15 15:44:07 +0600

    Basic exceptions and exception handler were added

---
+++ src/main/java/dev/dani/strong/newsfeed/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,41 @@
+package dev.dani.strong.newsfeed.exception;
+
+import dev.dani.strong.newsfeed.exception.exceptions.ResourceAlreadyExistException;
+import dev.dani.strong.newsfeed.exception.exceptions.ResourceNotFoundException;
+import dev.dani.strong.newsfeed.model.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
+        var responseBody = constructResponse(exception, NOT_FOUND);
+        return ResponseEntity.status(NOT_FOUND).body(responseBody);
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistException.class)
+    public ResponseEntity<ErrorResponse> resourceAlreadyExistExceptionHandler(ResourceAlreadyExistException exception) {
+        var responseBody = constructResponse(exception, CONFLICT);
+        return ResponseEntity.status(CONFLICT).body(responseBody);
+    }
+
+    private ErrorResponse constructResponse(RuntimeException exception, HttpStatus status) {
+        return ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(status)
+                .statusCode(status.value())
+                .message(exception.getMessage())
+                .build();
+    }
+
+}
---
+++ src/main/java/dev/dani/strong/newsfeed/exception/exceptions/ResourceAlreadyExistException.java
@@ -0,0 +1,23 @@
+package dev.dani.strong.newsfeed.exception.exceptions;
+
+public class ResourceAlreadyExistException extends RuntimeException {
+
+    public ResourceAlreadyExistException() {
+    }
+
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public ResourceAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceAlreadyExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResourceAlreadyExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
