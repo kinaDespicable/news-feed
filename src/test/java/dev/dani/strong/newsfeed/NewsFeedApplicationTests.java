164e0a7bdc4749abe7a9084e9ccba32668eff207
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2023-05-15 14:09:58 +0600

    first commit

---
+++ src/test/java/dev/dani/strong/newsfeed/NewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dani.strong.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
